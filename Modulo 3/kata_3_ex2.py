fe3d04e11ec83949e803519b87169883bbbc3a44
MarioSandovalC <99110531+MarioSandovalC@users.noreply.github.com>
2022-02-10 20:15:54 -0600

    Programas Kata3

---
+++ Modulo 3/kata_3_ex2.py
@@ -0,0 +1,15 @@
+# Si un asteroide entra en la atmósfera de la Tierra a una velocidad mayor o igual a 20 km/s,
+# a veces produce un rayo de luz que se puede ver desde la Tierra. 
+# Escribe la lógica condicional que usa declaraciones if, else, y elif para 
+# alertar a las personas de todo el mundo que deben buscar un asteroide en el cielo. 
+# ¡Hay uno que se dirige a la tierra ahora a una velocidad de 19 km/s!
+
+
+asteroide = 19
+
+if asteroide > 20: 
+    print("Hay un asteoride en el cielo")
+elif asteroide == 20:
+    print("Hay un asteoride en el cielo")
+else:
+    print("Vive tu vida se feliz")
